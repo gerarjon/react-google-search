362b0ec31be6a557c4a72181bd25f93ce2e354a7
gerarjon <gerarsuaverdez@gmail.com>
2020-03-26 13:56:15 -0700

    SearchResults returns title, description, image, and author

--- client/src/Components/SearchResults/index.js
+++ client/src/Components/SearchResults/index.js
@@ -4,13 +4,23 @@ import './style.css';
 const SearchResults = (props) => {
     return(
         <div>
-            <ul>
-                {props.results.map(result => (
-                    <li key='result'>
-                        <p>{result.volumeInfo.title}</p>
-                    </li>
-                ))}
-            </ul>
+            <h5>Results</h5>
+            <div className="row">
+                <ul>
+                    {props.results.map(result => (
+                        <li key='result' className="row">
+                            <div className="row">
+                                <h5>{result.volumeInfo.title}</h5>
+                            </div>
+                            <h6>Author(s): {result.volumeInfo.authors}</h6>
+                            <div className="row">
+                                <img className="col m2" alt={result.volumeInfo.title} src={result.volumeInfo.imageLinks === undefined ? "" : result.volumeInfo.imageLinks.thumbnail} />
+                                <p className="col m10">{result.volumeInfo.description === undefined ? "No description" : result.volumeInfo.description}</p>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
