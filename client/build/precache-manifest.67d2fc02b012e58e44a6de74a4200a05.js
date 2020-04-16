35deb9d08695aba486861cd8ef02d70b341c7d11
gerarjon <gerarsuaverdez@gmail.com>
2020-04-15 19:33:45 -0700

    added build folder

---
+++ client/build/precache-manifest.67d2fc02b012e58e44a6de74a4200a05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "90f19da3927541e2ca2c",
+    "url": "/static/js/main.b9e80e85.chunk.js"
+  },
+  {
+    "revision": "46f198adf02806d53478",
+    "url": "/static/js/2.a2af148e.chunk.js"
+  },
+  {
+    "revision": "90f19da3927541e2ca2c",
+    "url": "/static/css/main.7747c74c.chunk.css"
+  },
+  {
+    "revision": "7626f20e661069952e3b25ebb74fd05a",
+    "url": "/index.html"
+  }
+];
