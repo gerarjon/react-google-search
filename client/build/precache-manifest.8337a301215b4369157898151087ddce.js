8971ae173e994b0b95e48c87e57d8d4b61fcd8ec
gerarjon <gerarsuaverdez@gmail.com>
2020-04-15 20:19:10 -0700

    added build

---
+++ client/build/precache-manifest.8337a301215b4369157898151087ddce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f6e0309a960d102447e4",
+    "url": "/static/js/main.0224342b.chunk.js"
+  },
+  {
+    "revision": "f5a0adc8d0bc3fbf7243",
+    "url": "/static/js/2.5145a938.chunk.js"
+  },
+  {
+    "revision": "f6e0309a960d102447e4",
+    "url": "/static/css/main.78b3436e.chunk.css"
+  },
+  {
+    "revision": "60c8f5bc249fa43c51b6614c275fb52f",
+    "url": "/index.html"
+  }
+];
